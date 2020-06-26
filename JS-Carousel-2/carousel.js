27987a70a432f119b2a0b8a5d19b0ab8b90d5a6f
Lucky Shrestha <luckshrestha@gmail.com>
2020-06-26 19:02:17 +0545

    Add class to carousel

--- JS-Carousel-2/carousel.js
+++ JS-Carousel-2/carousel.js
@@ -1,125 +1,139 @@
-var carouselContainer = document.querySelector('.carousel-container');
-var carouselWrapper = document.querySelector('.carousel-image-wrapper');
+var Carousel = function (carouselId) {
 
-var images = carouselWrapper.children;
-var totalImages = images.length;
-var animationId;
-var percentage = 5;
+  var self = this;
 
-// active default to first image
-var activeIndex = 0;
-var activeImage = images[activeIndex];
-activeImage.setAttribute('class', 'active');
+  this.carouselContainer = document.getElementById(carouselId);
+  this.carouselWrapper = this.carouselContainer.querySelector('.carousel-image-wrapper');
 
-for (var i = 0; i < totalImages; i++) {
-  images[i].setAttribute('data-id', i);
-}
+  // get image elements in carousel wrapper 
+  this.images = this.carouselWrapper.children;
+  this.totalImages = this.images.length;
 
-function animate(fromImage, toImage, toDirection) {
+  // default transition of carousel
+  this.percentage = 5;
+  this.animationId;
 
-  var toImageInitalPosition = toDirection === 'left' ? 100 : -100;
-  var fromImageInitialPosition = 0;
-  var speed = toDirection === 'left' ? -percentage : percentage;
+  // active default to first image
+  this.activeIndex = 0;
+  this.activeImage = this.images[this.activeIndex];
+  this.activeImage.setAttribute('class', 'active');
 
-  function animateCallback() {
-    if (toImageInitalPosition !== 0) {
-      toImageInitalPosition += speed;
-      fromImageInitialPosition += speed;
-      toImage.style.left = toImageInitalPosition + '%';
-      fromImage.style.left = fromImageInitialPosition + '%';
-      requestAnimationFrame(animateCallback);
-    } else {
-      cancelAnimationFrame(animationId);
-      toImage.classList.remove('right', 'left');
-      toImage.removeAttribute('style');
-      fromImage.removeAttribute('class');
-      fromImage.removeAttribute('style');
-      refreshIndicator();
+  this.prevButton = document.createElement('button');
+  this.nextButton = document.createElement('button');
+  this.indicatorWrapper = document.createElement('div');
+
+  for (var i = 0; i < this.totalImages; i++) {
+    this.images[i].setAttribute('data-id', i);
+  }
+
+  function animate(fromImage, toImage, toDirection) {
+    var toImageInitalPosition = toDirection === 'left' ? 100 : -100;
+    var fromImageInitialPosition = 0;
+    var speed = toDirection === 'left' ? -this.percentage : this.percentage;
+
+    function animateCallback() {
+      if (toImageInitalPosition !== 0) {
+        toImageInitalPosition += speed;
+        fromImageInitialPosition += speed;
+        toImage.style.left = toImageInitalPosition + '%';
+        fromImage.style.left = fromImageInitialPosition + '%';
+        requestAnimationFrame(animateCallback);
+      } else {
+        cancelAnimationFrame(this.animationId);
+        toImage.classList.remove('right', 'left');
+        toImage.removeAttribute('style');
+        fromImage.removeAttribute('class');
+        fromImage.removeAttribute('style');
+        refreshIndicator.call(self);
+      }
+    }
+
+    this.animationId = requestAnimationFrame(animateCallback.bind(this));
+  }
+
+  function slideLeft() {
+    var toLeft = this.images[this.activeIndex];
+    var nextIndex = this.activeIndex + 1;
+
+    nextIndex %= this.totalImages;
+    var toActive = this.images[nextIndex];
+
+    toActive.classList.add('active', 'right');
+
+    animate.call(self, toLeft, toActive, 'left');
+
+    this.activeIndex = nextIndex;
+  }
+
+  function slideRight() {
+    var toRight = this.images[this.activeIndex];
+    var nextIndex = this.activeIndex - 1;
+
+    if (nextIndex < 0) {
+      nextIndex = this.totalImages + nextIndex;
+    }
+
+    var toActive = this.images[nextIndex];
+
+    toActive.classList.add('active', 'left');
+
+    animate.call(self, toRight, toActive, 'right');
+
+    this.activeIndex = nextIndex;
+  }
+
+  function renderNavigationButtons() {
+    this.prevButton.classList.add('carousel-btn', 'prev');
+    this.prevButton.innerHTML = '&#10094';
+    this.carouselContainer.appendChild(this.prevButton);
+
+    this.nextButton.classList.add('carousel-btn', 'next');
+    this.nextButton.innerHTML = '&#10095'
+    this.carouselContainer.appendChild(this.nextButton);
+  }
+
+  this.prevButton.onclick = slideRight.bind(self);
+  this.nextButton.onclick = slideLeft.bind(self);
+
+  function renderIndicators() {
+    this.indicatorWrapper.classList.add('carousel-indicator');
+    this.carouselContainer.appendChild(this.indicatorWrapper);
+
+    for (var i = 0; i < this.totalImages; i++) {
+      var indicator = document.createElement('div');
+      indicator.classList.add('indicator');
+      indicator.setAttribute('data-id', i);
+      this.indicatorWrapper.appendChild(indicator);
     }
   }
 
-  animationId = requestAnimationFrame(animateCallback);
-}
+  function refreshIndicator() {
 
-function slideLeft() {
+    if (this.indicatorWrapper.querySelector('.active')) {
+      this.indicatorWrapper.querySelector('.active').classList.remove('active');
+    }
 
-  var toLeft = images[activeIndex];
-  var nextIndex = activeIndex + 1;
-
-  nextIndex %= totalImages;
-  var toActive = images[nextIndex];
-
-  toActive.classList.add('active', 'right');
-
-  animate(toLeft, toActive, 'left');
-
-  activeIndex = nextIndex;
-}
-
-function slideRight() {
-
-  var toRight = images[activeIndex];
-  var nextIndex = activeIndex - 1;
-
-  if (nextIndex < 0) {
-    nextIndex = totalImages + nextIndex;
+    var activeIndicator = this.images[this.activeIndex].getAttribute('data-id');
+    this.indicatorWrapper.children.item(activeIndicator).classList.add('active');
   }
 
-  var toActive = images[nextIndex];
+  this.indicatorWrapper.onclick = function (event) {
+    if (event.srcElement.classList.contains('indicator')) {
+      var clickedIndex = parseInt(event.srcElement.getAttribute('data-id'));
+      var clickedImage = self.images[clickedIndex];
 
-  toActive.classList.add('active', 'left');
-
-  animate(toRight, toActive, 'right');
-
-  activeIndex = nextIndex;
-}
-
-var prevButton = document.createElement('button');
-prevButton.classList.add('carousel-btn', 'prev');
-prevButton.innerHTML = '&#10094';
-carouselContainer.appendChild(prevButton);
-
-var nextButton = document.createElement('button');
-nextButton.classList.add('carousel-btn', 'next');
-nextButton.innerHTML = '&#10095'
-carouselContainer.appendChild(nextButton);
-
-prevButton.onclick = slideRight;
-nextButton.onclick = slideLeft;
-
-var indicatorWrapper = document.createElement('div');
-indicatorWrapper.classList.add('carousel-indicator');
-
-carouselContainer.appendChild(indicatorWrapper);
-
-for (var i = 0; i < totalImages; i++) {
-  var indicator = document.createElement('div');
-  indicator.classList.add('indicator');
-  indicator.setAttribute('data-id', i);
-  indicatorWrapper.appendChild(indicator);
-}
-
-function refreshIndicator() {
-
-  if (indicatorWrapper.querySelector('.active')) {
-    indicatorWrapper.querySelector('.active').classList.remove('active');
-  }
-
-  var activeIndicator = carouselWrapper.children[activeIndex].getAttribute('data-id');
-  indicatorWrapper.children.item(activeIndicator).classList.add('active');
-}
-
-refreshIndicator();
-
-indicatorWrapper.onclick = function (event) {
-  if (event.srcElement.classList.contains('indicator')) {
-    var clickedIndex = Number(event.srcElement.getAttribute('data-id'));
-    var clickedImage = images[clickedIndex];
-
-    if (!clickedImage.classList.contains('active')) {
-      clickedImage.classList.add('active', 'right');
-      animate(images[activeIndex], clickedImage, 'left');
-      activeIndex = clickedIndex;
+      if (!clickedImage.classList.contains('active')) {
+        clickedImage.classList.add('active', 'right');
+        animate.call(self, self.images[self.activeIndex], clickedImage, 'left');
+        self.activeIndex = clickedIndex;
+      }
     }
   }
+
+  this.render = function () {
+    renderNavigationButtons.call(self);
+    renderIndicators.call(self);
+    refreshIndicator.call(self);
+  }
+
 }

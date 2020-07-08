1dbcc54dad1481565e16e42e3ba17e4a6a66731c
Lucky Shrestha <luckshrestha@gmail.com>
2020-07-08 15:53:21 +0545

    Fix ball bounce frame issue and Add ball start height

--- Ping-Pong/scripts/ball.js
+++ Ping-Pong/scripts/ball.js
@@ -2,9 +2,9 @@ class Ball {
   constructor(startPos) {
     this.intial3dPos = new Position(startPos.x, startPos.y, startPos.z);
     this.current3dPos = new Position(startPos.x, startPos.y, startPos.z);
-    this.radius = 24;
-    this.angle = 60 * Math.PI / 180;
-    this.intialVel = 50;
+    this.radius = 24; // default radius of ball
+    this.angle = 45 * Math.PI / 180; // drive ball with 45 degree angle with the horizontal plane
+    this.intialVel = 60;
     this.velocity = {
       'z': this.intialVel * Math.cos(this.angle),
       'y': 0
@@ -18,57 +18,44 @@ class Ball {
   }
 
   draw = () => {
-    let current2dPos = projection.get2dProjection(this.current3dPos);
+    // let current2dPos = projection.get2dProjection(this.current3dPos);
+    let current2dPos = projection.get2dProjection(this.current3dPos.reflectXAxis());
+
+    this.radius = current2dPos.y * 0.03;
 
     ctx.beginPath();
     ctx.arc(current2dPos.x, current2dPos.y, this.radius, 0, 360);
     ctx.fillStyle = "#000";
     ctx.fill();
     ctx.closePath();
-
-    console.log(current2dPos.x, -current2dPos.y);
   }
 
   bounce = () => {
     if (!this.rebound) {
-      let current2dPos = projection.get2dProjection(this.current3dPos.reflectXAxis());
-      this.radius = current2dPos.y * 0.03;
 
-      ctx.beginPath();
-      ctx.arc(current2dPos.x, current2dPos.y, this.radius, 0, 360);
-      ctx.fillStyle = "#000";
-      ctx.fill();
-      ctx.closePath();
-
+      this.draw();
       this.current3dPos.z = this.intial3dPos.z + this.velocity.z * this.time;
-
-      // console.log(this.current3dPos.z);
 
       let vy = this.intialVel * Math.sin(this.angle);
 
       this.velocity.y = vy - ENV.gravity * this.time;
       this.current3dPos.y = this.intial3dPos.y + (vy * this.time) - (ENV.gravity * this.time * this.time * 0.5);
 
-      // debugger
-
-      if (this.current3dPos.y < this.intial3dPos.y) {
-        // console.log(this.current3dPos.z, this.current3dPos.y);
+      if (this.current3dPos.y < -BALL_START_HEIGHT * 2) {
         this.rebound = true;
-        // console.log('bounce');
       }
 
       this.time += 0.1;
 
     } else {
-
       this.intialVel = -this.velocity.y;
       this.intial3dPos.z = this.current3dPos.z;
-      this.current3dPos.y = this.intial3dPos.y;
-
+      this.current3dPos.y = -BALL_START_HEIGHT * 2;
+      this.intial3dPos.y = -BALL_START_HEIGHT * 2;
       this.velocity.z = this.intialVel * Math.cos(this.angle);
-
       this.rebound = false;
       this.time = 0;
+      this.draw();
     }
   }
 
--- Ping-Pong/scripts/index.js
+++ Ping-Pong/scripts/index.js
@@ -7,6 +7,8 @@ const canvasWidth = canvas.width;
 const canvasHeight = canvas.height;
 const ctx = canvas.getContext('2d');
 
+const BALL_START_HEIGHT = 200; // vertical height from the board
+
 let animationId;
 const halfCanvasWidth = canvasWidth / 2;
 
@@ -14,7 +16,7 @@ projection.camera.position.x = halfCanvasWidth;
 projection.viewplane.x = halfCanvasWidth;
 
 
-// persective sideview
+// persective sideview inverted
 // projection.camera.position.x = -100;
 // projection.camera.position.y = -100;
 // projection.camera.position.z = 1060;
@@ -22,16 +24,15 @@ projection.viewplane.x = halfCanvasWidth;
 // ctx.translate(0, canvasHeight/2);
 
 const table = new Board();
-const ball = new Ball(new Position(halfCanvasWidth, -16, table.z));
+const ball = new Ball(new Position(halfCanvasWidth, -BALL_START_HEIGHT, table.z));
 
 table.draw();
 ball.draw();
 
-
 console.log(table);
 console.log(ball);
 
-function render(){
+function render() {
   ctx.clearRect(-500, -500, canvasWidth + 500, canvasHeight + 500);
   table.draw();
   ball.bounce();

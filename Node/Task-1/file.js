67347b94665457469cf91e96de9513b208c0405e
Lucky Shrestha <luckshrestha@gmail.com>
2020-08-10 21:11:39 +0545

    Add files inside directory

--- Node/Task-1/file.js
+++ Node/Task-1/file.js
@@ -1,5 +1,28 @@
 const fs = require('fs');
 
+function makeDir(path) {
+
+  return new Promise((resolve, reject) => {
+
+    if (fs.existsSync(path)) resolve('directory already exists');
+
+    fs.mkdir(path, (err) => {
+      if (err) reject(err);
+      resolve('create directory successful');
+    });
+  });
+}
+
+function readDir(path) {
+
+  return new Promise((resolve, reject) => {
+    fs.readdir(path, (err, files) => {
+      if (err) reject(err);
+      resolve(files);
+    })
+  })
+}
+
 function writeFile(path, data) {
 
   return new Promise((resolve, reject) => {
@@ -41,8 +64,10 @@ function deleteFile(path) {
 }
 
 module.exports = {
+  makeDir,
+  readDir,
   writeFile,
   readFile,
   renameFile,
-  deleteFile
+  deleteFile,
 }
--- Node/Task-1/index.js
+++ Node/Task-1/index.js
@@ -1,51 +1,77 @@
 const http = require('http');
+const path = require('path');
 const fileOperation = require('./file');
 
 const host = '127.0.0.1';
 const port = 1234;
+const dir = './files';
+
+/*
+Endpoints to:
+write a file - http://127.0.0.1/write/<filename.extension>/<content>
+read a file - http://127.0.0.1/read/<filename.extension>
+rename a file - http://127.0.0.1/rename/<oldFilename.extension>/<newFilename.extension>
+delete a file - http://127.0.0.1/delete/<filename.extension>
+*/
 
 const server = http.createServer((req, res) => {
   if (req.method === 'GET') {
 
     const resSuccess = message => res.end(message);
     const resError = error => res.end(error.message);
-    
+
     res.setHeader('Content-Type', 'text/plain');
 
     const [action, file, data] = req.url.split('/').splice(1);
 
+    let filePath;
+
+    if (file) filePath = path.join(dir, file);
+
     switch (action) {
 
       case 'write':
-        fileOperation.writeFile(file, data)
+        fileOperation.writeFile(filePath, data)
           .then(resSuccess)
           .catch(resError);
         break;
 
       case 'read':
-        fileOperation.readFile(file)
+        fileOperation.readFile(filePath)
           .then(resSuccess)
           .catch(resError);
         break;
 
       case 'rename':
-        fileOperation.renameFile(file)
+        const newPath = path.join(dir, data);
+
+        fileOperation.renameFile(filePath, newPath)
           .then(resSuccess)
           .catch(resError);
         break;
 
       case 'delete':
-        fileOperation.deleteFile(file)
+        fileOperation.deleteFile(filePath)
           .then(resSuccess)
           .catch(resError);
         break;
 
       default:
-        res.end('undefined action!');
+        fileOperation.readDir(dir)
+          .then(result => {
+
+            let savedFiles = '';
+            result.forEach(file => savedFiles += `${file}\n`);
+            resSuccess(savedFiles);
+          })
+          .catch(resError);
     }
   }
 });
 
 server.listen(port, host, () => {
+
+  fileOperation.makeDir(dir).then(message => console.log(message));
   console.log(`listening to http://${host}:${port}`)
+
 });
